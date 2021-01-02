ca69004884950fe9198712892a69f54cdc4d3904
Hector Solans <hectors@gmail.com>
2021-01-02 16:24:49 +0100

    Add chart Memory

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    title = 'Test'
+    title = 'Check Stats'
     context = {}
-    for line in query_db('select * from ecs_cluster_blocs_pro'):
-        context[line[0]]={'cpu':line[1],'task':line[2]}
+    for line in query_db("select * from ecs_cluster_blocs_pro WHERE TIME BETWEEN '2021-01-02 10:45' AND '2021-01-02 15:00'"):
+        cpu = (line[1] if line[1] else 0)
+        task = (line[2] if line[2] else 0)
+        mem = (line[3] if line[3] else 0)
+        context[line[0]]={'cpu':cpu,'task':task,'mem':mem}
     return render_template('char.html', title=title, context=context)
 
 @app.teardown_appcontext
