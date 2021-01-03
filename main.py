9fbe30b6ca615e342301c9fc8961a9a7ff3693fe
Hector Solans <hectors@gmail.com>
2021-01-03 11:21:10 +0100

    Add periods on char

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
 
 import settings
-from flask import Flask, g, render_template
+from flask import Flask, g, render_template, redirect, url_for
 from functions import get_db, query_db
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    title = 'Check Stats'
+@app.route('/<period>')
+def graphs(period):
+    periods = {'3h': '-3 hours', '6h': '-6 hours', '12h': '-12 hours', '1d': '-1 days', '3d': '-3 days', '1w': '-7 days', '1m': '-1 month',}
+    period = periods.get(period, '-1 days')
     context = {}
-    for line in query_db("select * from ecs_cluster_blocs_pro WHERE TIME BETWEEN '2021-01-02 10:45' AND '2021-01-02 15:00'"):
+    sql = "select TIME,CPU,TASKCOUNT,MEM from ecs_cluster_blocs_pro WHERE TIME > datetime('now', '" + period + "')"
+    for line in query_db(sql):
         cpu = (line[1] if line[1] else 0)
         task = (line[2] if line[2] else 0)
         mem = (line[3] if line[3] else 0)
         context[line[0]]={'cpu':cpu,'task':task,'mem':mem}
-    return render_template('char.html', title=title, context=context)
+    return render_template('char.html', title='Graph Stats', context=context)
+
+@app.route('/')
+def index():
+    return redirect(url_for('graphs',period='1d'))
 
 @app.teardown_appcontext
 def close_connection(exception):
