c33a0b20a6075fab671de02da4d2b640dc3d69bf
Hector Solans <hectors@gmail.com>
2021-01-04 11:26:38 +0100

    First Release

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ import settings
 from flask import Flask, g, render_template, redirect, url_for
 from functions import get_db, query_db
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='/ecs/blocs-pro/static')
 
-@app.route('/<period>')
+@app.route('/ecs/blocs-pro/<period>')
 def graphs(period):
     periods = {'3h': '-3 hours', '6h': '-6 hours', '12h': '-12 hours', '1d': '-1 days', '3d': '-3 days', '1w': '-7 days', '1m': '-1 month',}
-    period = periods.get(period, '-1 days')
+    sqlperiod = periods.get(period, '-1 days')
     context = {}
-    sql = "select TIME,CPU,TASKCOUNT,MEM from ecs_cluster_blocs_pro WHERE TIME > datetime('now', '" + period + "')"
+    sql = "select TIME,CPU,TASKCOUNT,MEM from ecs_cluster_blocs_pro WHERE TIME > datetime('now', '" + sqlperiod + "')"
     for line in query_db(sql):
         cpu = (line[1] if line[1] else 0)
         task = (line[2] if line[2] else 0)
         mem = (line[3] if line[3] else 0)
         context[line[0]]={'cpu':cpu,'task':task,'mem':mem}
-    return render_template('char.html', title='Graph Stats', context=context)
+    return render_template('char.html', title='Graph Stats', period=period, periods=periods, context=context)
 
-@app.route('/')
+@app.route('/ecs/blocs-pro/')
 def index():
     return redirect(url_for('graphs',period='1d'))
 
