3b292797ab2da5e91135fde9df5637142fa27308
Hector Solans <hectors@gmail.com>
2021-01-07 11:28:59 +0100

    Look and Feel

--- functions.py
+++ functions.py
@@ -4,7 +4,8 @@ import datetime
 from pytz import timezone
 from os import path, getcwd
 
-periods = {'1d': 'end-1d', '1w': 'end-1w', '1m': 'end-1m',}
+# periods = {'1d': 'end-1d', '1w': 'end-1w', '1m': 'end-1m',}
+periods = {'1d': ['end-1d','Daily'], '1w': ['end-1w','Weekly'], '1m': ['end-1m','Monthly']}
 metrics = ['cpu','mem','task']
 
 fmt = "%d-%m-%Y %H\:%M"
@@ -19,16 +20,22 @@ strdate = now_timezone.strftime(fmt)
 filepath = path.abspath(getcwd())
 filepath += '/rrdtool/img/'
 
-def createrrdimagecpu(rrdfile, period='1d'):    
+def createrrdimagecpu(rrdfile, period='1d'):  
     fileimage = filepath + 'cpu-' + period + '.png'
+    period = periods.get(period)  
     try:
-        rrdtool.graph(str(fileimage), "-s", "%s" % periods.get(period), "-e", "now",
+        rrdtool.graph(str(fileimage), "-s", "%s" % period[0], "-e", "now",
+            "--imgformat=PNG",
             "--width=380", "--height=140", "--rigid",
             '--alt-autoscale-max', '--lower-limit=0',
-            "--title=CPU Metrics",
+            "--title=CPU Metrics %s" % period[1],
             "--color=CANVAS#222225",
             "--color=FONT#FFFFFF",
             "--color=BACK#222225",
+            "--font=TITLE:10:",
+            "--font=UNIT:8:",
+            "--watermark=Bekodo",
+            "--border=0",
             "DEF:cpu=%s:cpu:AVERAGE" % rrdfile,
             "LINE1:cpu#00CF00FF:Cpu",
             "AREA:cpu#00CF0033",
@@ -42,14 +49,20 @@ def createrrdimagecpu(rrdfile, period='1d'):
 
 def createrrdimagemem(rrdfile, period='1d'):
     fileimage = filepath + 'mem-' + period + '.png'
+    period = periods.get(period)  
     try:
-        rrdtool.graph(str(fileimage), "-s", "%s" % periods.get(period), "-e", "now",
+        rrdtool.graph(str(fileimage), "-s", "%s" % period[0], "-e", "now",
+            "--imgformat=PNG",
             "--width=380", "--height=140", "--rigid",
             '--alt-autoscale-max', '--lower-limit=0',
-            "--title=Memory Metrics",
+            "--title=Memory Metrics %s" % period[1],
             "--color=CANVAS#222225",
             "--color=FONT#FFFFFF",
             "--color=BACK#222225",
+            "--font=TITLE:10:",
+            "--font=UNIT:8:",
+            "--watermark=Bekodo",
+            "--border=0",
             "DEF:mem=%s:mem:AVERAGE" % rrdfile,
             "LINE1:mem#005199FF:Mem",
             "AREA:mem#00519933",
@@ -63,14 +76,20 @@ def createrrdimagemem(rrdfile, period='1d'):
 
 def createrrdimagetask(rrdfile, period='1d'):
     fileimage = filepath + 'task-' + period + '.png'
+    period = periods.get(period)  
     try:
-        rrdtool.graph(str(fileimage), "-s", "%s" % periods.get(period), "-e", "now",
+        rrdtool.graph(str(fileimage), "-s", "%s" % period[0], "-e", "now",
+            "--imgformat=PNG",
             "--width=380", "--height=140", "--rigid",
             '--alt-autoscale-max', '--lower-limit=0',
-            "--title=Tasks Metrics",
+            "--title=Tasks Metrics %s" % period[1],
             "--color=CANVAS#222225",
             "--color=FONT#FFFFFF",
             "--color=BACK#222225",
+            "--font=TITLE:10:",
+            "--font=UNIT:8:",
+            "--watermark=Bekodo",
+            "--border=0",
             "DEF:task=%s:task:AVERAGE" % rrdfile,
             "LINE1:task#008199FF:Task",
             "AREA:task#00819933",
