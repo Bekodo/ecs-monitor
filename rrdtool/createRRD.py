d98ec3c9b460f4ed34b38d194581e574567a7d4f
Hector Solans <hectors@gmail.com>
2021-01-11 13:06:11 +0100

    add meta no-cache

--- rrdtool/createRRD.py
+++ rrdtool/createRRD.py
@@ -4,6 +4,7 @@ import rrdtool
 
 #Reference https://apfelboymchen.net/gnu/rrd/create/
 #http://rrdwizard.appspot.com/
+#https://github.com/octo/rrdtool/blob/master/doc/rrdtutorial.es.pod
 
 rrdtool.create("ecs_mem_cpu_task.rrd",
 "--step", "300",
